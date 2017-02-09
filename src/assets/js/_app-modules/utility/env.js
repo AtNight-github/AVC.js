ec24cf344ba7694f5e833083ac9e521f60df8cca
NaohiroUrayama <formes@urayamanaodai-no-iMac.local>
2017-02-09 15:14:04 +0900

    env途中

--- src/assets/js/_app-modules/utility/env.js
+++ src/assets/js/_app-modules/utility/env.js
@@ -1,44 +1,85 @@
 let ua = navigator.userAgent.toLowerCase();
 let ver = navigator.appVersion.toLowerCase();
+const uaParser = new UAParser();
 
 class Env{
     constructor() {
-        //BROWSER
-       let isMSIE = (ua.indexOf('msie') > -1) && (ua.indexOf('opera') == -1);
-       this.isIE6 = isMSIE && (ver.indexOf('msie 6.') > -1);
-       this.isIE7 = isMSIE && (ver.indexOf('msie 7.') > -1);
-       this.isIE8 = isMSIE && (ver.indexOf('msie 8.') > -1);
-       this.isIE9 = isMSIE && (ver.indexOf('msie 9.') > -1);
-       this.isIE10 = isMSIE && (ver.indexOf('msie 10.') > -1);
-       this.isIE11 = (ua.indexOf('trident/7') > -1);
-       this.isEdge = (ua.indexOf('edge') > -1);
-       this.isChrome = (ua.indexOf('chrome') > -1) && (ua.indexOf('edge') == -1);
-       this.isFirefox = (ua.indexOf('firefox') > -1);
-       this.isSafari = (ua.indexOf('safari') > -1) && (ua.indexOf('chrome') == -1);
-       this.isOpera = (ua.indexOf('opera') > -1);
+        this.browser = {};
+        this.device = {};
+        this.engine = {};
+        this.os = {};
+        this.support = {};
 
-       //DEVICE
-       this.isMobile = ua.indexOf('iphone') > 0 || ua.indexOf('ipod') > 0 || ua.indexOf('android') > 0 && ua.indexOf('mobile') > 0;
-       this.isTablet = ua.indexOf('ipad') > 0 || ua.indexOf('android') > 0;
+        //BROWSER
+        const browser = uaParser.getResult().browser;
+
+
+        console.info(browser);
+
+        this.browser.isIE = (browser.name === 'IE');
+
+        this.browser.isIE9 = (this.browser.isIE && browser.version === '9.0');
+        this.browser.isIE10 = (this.browser.isIE && browser.version === '10.0');
+        this.browser.isIE11 = (this.browser.isIE && browser.version === '11.0');
+        this.browser.isEdge = (browser.name === 'Edge');
+        this.browser.isChrome = (browser.name === 'Chrome');
+        this.browser.isFirefox = (browser.name === 'Firefox');
+        this.browser.isSafari = (browser.name === 'Safari');
+        this.browser.isMobileSafari = (browser.name === 'Mobile Safari');
+        this.browser.isAndroidBrowser = (browser.name === 'Android Browser');
+
+        //DEVICE
+        const device = uaParser.getResult().device;
+        this.device.isMobile = (device.type === 'mobile');
+        this.device.isTablet = (device.type === 'tablet');
+        this.device.isDesktop = (!this.device.isMobile && !this.device.isTablet);
+        this.device.isIphone = (device.model === 'iPhone');
+        this.device.isIpad = (device.model === 'iPad');
 
         //ENGINE
-
-
+        const engine = uaParser.getResult().engine;
+        this.engine.isGecko = (engine.name === 'Gecko');
+        this.engine.isWebKit = (engine.name === 'WebKit');
+        this.engine.isTrident = (engine.name === 'Trident');
+        this.engine.isEdgeHTML = (engine.name === 'EdgeHTML');
 
         //OS
+        const os = uaParser.getResult().os;
+        this.os.isIOS = (os.name === 'iOS');
+        this.os.isAndroidOS = (os.name === 'Android');
+        this.os.isWindows = (os.name === 'Windows');
+        this.os.isMac = (os.name === 'Mac OS');
 
         //SUPPORT
-        // this.isSupport = {
-        //     canvas: modernizr.canvas,
-        //     cssAnimation: modernizr.cssanimations,
-        //     cssTransition: modernizr.csstransitions,
-        //     hashchange: modernizr.hashchange,
-        //     history: modernizr.history,
-        //     requestAnimationFrame: modernizr.requestanimationframe,
-        //     svg: modernizr.svg,
-        //     touchEvent: modernizr.touchevents,
-        //     webgl: modernizr.webgl
-        // };
+        const detectCSSFeature = (featurename)=>{
+            var feature = false,
+                domPrefixes = 'Webkit Moz ms O'.split(' '),
+                elm = document.createElement('div'),
+                featurenameCapital = null;
+
+                featurename = featurename.toLowerCase();
+
+                if( elm.style[featurename] !== undefined ) { feature = true; }
+
+                if( feature === false ) {
+                    featurenameCapital = featurename.charAt(0).toUpperCase() + featurename.substr(1);
+                    for( var i = 0; i < domPrefixes.length; i++ ) {
+                        if( elm.style[domPrefixes[i] + featurenameCapital ] !== undefined ) {
+                          feature = true;
+                          break;
+                        }
+                    }
+                }
+                return feature;
+        };
+        this.support.canvas = (()=>{
+            let elem = document.createElement('canvas');
+            return !!(elem.getContext && elem.getContext('2d'));
+        })();
+        this.support.cssTransition = detectCSSFeature('transition');
+        this.support.cssAnimation = detectCSSFeature('animation');
+        this.support.hashchange = ("onhashchange" in window);
+        this.support.history = (window.history && window.history.pushState);
     }
 }
 
